9bd4f5af6c1db406a4e3d40fc9bee0d17a448b75
Paul-Cte <paul.comte3878@gmail.com>
2025-02-06 16:23:15 +0100

    dark theme

--- public/assets/script.js
+++ public/assets/script.js
@@ -4,10 +4,7 @@ const divoverlay = document.getElementsByClassName('divoverlay')[0];
 const body = document.getElementsByTagName('body')[0];
 const close_by_clic = document.getElementsByClassName('close-by-clic');
 
-// pour la page projet mais j'en ai besoin ici pour l'enlever quand le menu apparait
-const div_img_projets = document.querySelectorAll('.div-projets-img');
-const div_details = document.querySelectorAll('.details-projets');
-const btn_close = document.querySelectorAll('.fi-br-cross');
+
 
 btn_menu.addEventListener('click', () => {
     const isOpen = menu.classList.contains('active'); // Vérifie si le menu est ouvert
@@ -20,13 +17,6 @@ btn_menu.addEventListener('click', () => {
         
     } else {
         body.classList.add('bodyactive');
-        btn_close.forEach((btn, index) => {
-            div_details[index].classList.remove('details-projets-active');
-            div_details[index].classList.add('details-not-active');
-            divoverlay.classList.remove('overlay2');
-            body.classList.remove('bodyactive2');
-
-    });
     }
 });
 
@@ -45,40 +35,7 @@ if(close_by_clic.length>0){
 
 
 
-//pour la page projet
 
-
-
-// Utilisation de forEach pour itérer sur div_img_projets
-div_img_projets.forEach((img, index) => {
-    img.addEventListener('click', () => {
-        const isOpen = menu.classList.contains('active'); // Vérifie si le menu est ouvert
-        // On utilise l'index pour lier chaque div-img-projets à un div-details
-        div_details[index].classList.remove('details-not-active');
-        div_details[index].classList.add('details-projets-active');
-        divoverlay.classList.add('overlay2');
-        body.classList.add('bodyactive2');
-
-        if (isOpen) {
-            div_details[index].classList.add('details-not-active');
-            div_details[index].classList.remove('details-projets-active');
-            divoverlay.classList.remove('overlay2');
-            body.classList.remove('bodyactive2');
-        }   
-
-    });
-});
-
-btn_close.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        // Lorsque le bouton de fermeture est cliqué, on cache le div-details correspondant
-        div_details[index].classList.remove('details-projets-active');
-        div_details[index].classList.add('details-not-active');
-        divoverlay.classList.remove('overlay2');
-        body.classList.remove('bodyactive2');
-
-    });
-});
 
 
 
