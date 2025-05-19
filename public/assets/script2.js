ca16524f99e9920ac8f928c3239d0529f450b509
Paul-Cte <paul.comte3878@gmail.com>
2025-05-19 11:35:30 +0200

    cdb

--- public/assets/script2.js
+++ public/assets/script2.js
@@ -1,24 +1,14 @@
-const btn_menu = document.getElementsByClassName('btn-burger')[0];
-const menu = document.getElementsByClassName('liste-menu')[0];
-const body = document.getElementsByTagName('body')[0];
-const close_by_clic = document.getElementsByClassName('close-by-clic');
-const btn_fermeture_menu = document.getElementById('btn_fermeture');
+const btn_fermture_menu = document.getElementById('btn_fermeture');
 
 
 
 const closeMenu = () => {
-    menu.classList.remove('active');
-    body.classList.remove('bodyactive');
+    window.location.href = "https://paulcteportfolio.netlify.app/#goto";
 };
 
-btn_fermeture_menu.addEventListener('click',closeMenu)
+btn_fermture_menu.addEventListener('click',closeMenu)
 
 
 
-if(close_by_clic.length>0){
-    for(let i=0; i<close_by_clic.length; i++){
-        close_by_clic[i].addEventListener('click',closeMenu);
-    }
-}
 
 
