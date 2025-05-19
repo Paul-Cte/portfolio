4b147ddd74e4ead4f450ec39a83d5194dd61a397
Paul-Cte <paul.comte3878@gmail.com>
2025-05-19 11:24:50 +0200

    svppp

--- public/assets/script2.js
+++ public/assets/script2.js
@@ -2,30 +2,16 @@ const btn_menu = document.getElementsByClassName('btn-burger')[0];
 const menu = document.getElementsByClassName('liste-menu')[0];
 const body = document.getElementsByTagName('body')[0];
 const close_by_clic = document.getElementsByClassName('close-by-clic');
-const btn_fermture_menu = document.getElementById('btn_fermeture');
+const btn_fermeture_menu = document.getElementById('btn_fermeture');
 
 
-
-img_btn_burger.addEventListener('click', () => {
-    const isOpen = menu.classList.contains('active'); // Vérifie si le menu est ouvert
-    
-    menu.classList.toggle('active');
-
-    if (isOpen) {
-        body.classList.remove('bodyactive');
-        
-    } else {
-        body.classList.add('bodyactive');
-        
-    }
-});
 
 const closeMenu = () => {
     menu.classList.remove('active');
     body.classList.remove('bodyactive');
 };
 
-btn_fermture_menu.addEventListener('click',closeMenu)
+btn_fermeture_menu.addEventListener('click',closeMenu)
 
 
 
