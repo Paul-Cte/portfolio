f246a3ddb543a19fd0d585f7598bccf4b7c097c3
Paul-Cte <paul.comte3878@gmail.com>
2025-05-19 12:17:23 +0200

    all bug fix

--- public/assets/script2.js
+++
@@ -1,9 +0,0 @@
-  // const clickableDiv = document.getElementById("clickableDiv");
-
-  // // Ajoute un écouteur de clic
-  // clickableDiv.addEventListener("click", function() {
-  //   window.location.href = "https://paulcteportfolio.netlify.app/#goto"; // Redirection
-  // });
-
-
-
