406af5f9cb1be8d09fbf0099af91ad1dbb9af4b0
Paul-Cte <paul.comte3878@gmail.com>
2025-05-19 11:58:25 +0200

    retour home fixed bug

--- public/assets/script.js
+++ public/assets/script.js
@@ -60,6 +60,14 @@ document.querySelectorAll(".shareButton").forEach(button => {
 });
 
 
+if (window.innerWidth >= 750) {
+    const clickAbleDiv = document.getElementById("clickAbleDiv");
+
+    clickAbleDiv.addEventListener("click", function() {
+      window.location.href = "https://paulcteportfolio.netlify.app/";
+    });
+  }
+
 
 
 
