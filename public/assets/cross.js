e544d8e60d2a8856a29e62db26802400be207e29
Paul-Cte <paul.comte3878@gmail.com>
2025-05-22 15:23:53 +0200

    update

---
+++ public/assets/cross.js
@@ -0,0 +1,12 @@
+if (window.innerWidth >= 750) {
+    const clickAbleDiv = document.getElementById("clickAbleDiv");
+
+    clickAbleDiv.addEventListener("click", function() {
+      window.location.href = "https://paulcteportfolio.netlify.app/";
+    });
+  }
+
+
+
+
+  
--- public/assets/script.js
+++ public/assets/script.js
@@ -60,13 +60,7 @@ document.querySelectorAll(".shareButton").forEach(button => {
 });
 
 
-if (window.innerWidth >= 750) {
-    const clickAbleDiv = document.getElementById("clickAbleDiv");
 
-    clickAbleDiv.addEventListener("click", function() {
-      window.location.href = "https://paulcteportfolio.netlify.app/";
-    });
-  }
 
 const rond = document.getElementById('rond');
 
